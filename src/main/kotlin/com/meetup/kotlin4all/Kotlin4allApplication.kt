5481e67b6e521ed384af6f206d5cffbf9ad5cb77
Luís Soares <lsoares@gmail.com>
2019-03-28 22:30:36 +0000

    Add Spring

---
+++ src/main/kotlin/com/meetup/kotlin4all/Kotlin4allApplication.kt
@@ -0,0 +1,11 @@
+package com.meetup.kotlin4all
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin4allApplication
+
+fun main() {
+    runApplication<Kotlin4allApplication>()
+}
---
+++ src/test/kotlin/com/meetup/kotlin4all/Kotlin4allApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.meetup.kotlin4all
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Kotlin4allApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
