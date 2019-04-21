4d057a831e94ab4a616a9a5f655d579938472a91
Luís Soares <lsoares@gmail.com>
2019-04-21 11:35:19 +0100

    Add git ignore file

--- javalin/src/test/kotlin/IntegrationTest.kt
+++ javalin/src/test/kotlin/IntegrationTest.kt
@@ -3,16 +3,18 @@ import org.junit.jupiter.api.Test
 import java.net.URI
 import java.net.http.HttpClient.newHttpClient
 import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.net.http.HttpResponse.*
 
 class IntegrationTest {
 
     @Test
     fun `test hello world`() {
         main()
-        val request = HttpRequest.newBuilder().uri(URI("http://localhost:7000?name=meetup.com")).GET()
+        val request = HttpRequest.newBuilder()
+            .uri(URI("http://localhost:7000?name=meetup.com"))
+            .GET()
 
-        val httpResponse = newHttpClient().send(request.build(), HttpResponse.BodyHandlers.ofString())
+        val httpResponse = newHttpClient().send(request.build(), BodyHandlers.ofString())
 
         assertEquals("Hello meetup.com", httpResponse.body())
     }
